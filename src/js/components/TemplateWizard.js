004f19a7e0e3716cc08f5a29a7ce39eb1809ad19
Niklas Keller <me@kelunik.com>
2015-12-26 13:05:43 +0100

    Implement placeholders for templates

--- src/js/components/TemplateWizard.js
+++ src/js/components/TemplateWizard.js
@@ -1,22 +1,106 @@
 import React from "react";
 
+import Placeholder from "../placeholder";
+
+function getPlaceholderItem(placeholder) {
+    return (
+        <li className="placeholder">
+            {placeholder}
+        </li>
+    )
+}
+
+function getWarningItem(warning) {
+    return (
+        <div className="warning">
+            <i className="fa fa-warning"/> {warning}
+        </div>
+    )
+}
+
 class TemplateWizard extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            placeholders: {},
+            warnings: []
+        }
+    }
+
     render() {
+        // TODO: Find out how to use {{ }} in React literally.
+        let html = "You can add placeholders by using the following syntax: <code>{{placeholder-name:placeholder-type}}</code>";
+        let placeholderHelp = (
+            <div className="help">
+                <p dangerouslySetInnerHTML={{__html: html}}/>
+                <ul>
+                    <li><code>placeholder-name</code><br />
+                        You may use the following characters:
+                        <code>a-z</code>, <code>-</code>, <code>_</code>.
+                    </li>
+                    <li>
+                        <code>placeholder-type</code><br />
+                        You may use any <a href="https://developer.mozilla.org/de/docs/Web/HTML/Element/Input">input
+                        type defined in HTML 5</a>. Default: <code>text</code>.
+                    </li>
+                </ul>
+            </div>
+        );
+
+        let placeholders = [];
+        let warnings = [];
+        let placeholderLabel;
+
+        if (Object.keys(this.state.placeholders).length) {
+            placeholders = Object.keys(this.state.placeholders).map(getPlaceholderItem);
+            placeholderLabel = (
+                <label>Placeholders</label>
+            );
+        }
+
+        if (this.state.warnings.length) {
+            warnings = this.state.warnings.map(getWarningItem);
+        }
+
         return (
             <div className="wizard">
-                <label>Title</label>
-                <input type="text" placeholder="Title" name="title" />
+                <label>Name</label>
+                <input type="text" placeholder="Name" name="name"/>
 
-                <label>Description</label>
-                <textarea placeholder="Description" name="description" className="resize-horizontal" />
+                <label>Content</label>
+                <textarea placeholder="Content" name="content" ref="content"
+                          onChange={this._onContentChange.bind(this)}/>
+
+                {placeholderHelp}
+                {placeholderLabel}
+
+                <ul className="placeholders">
+                    {placeholders}
+                </ul>
+
+                <div className="warnings">
+                    {warnings}
+                </div>
 
                 <div className="actions actions-right">
                     <button type="submit" className="action action-constructive">
-                        <i className="fa fa-save" /> Save
+                        <i className="fa fa-save"/> Save
                     </button>
                 </div>
             </div>
         );
+    }
+
+    _onContentChange() {
+        let value = this.refs.content.value;
+        let placeholders = Placeholder.parse(value);
+        let warnings = Placeholder.check(placeholders);
+
+        this.setState({
+            placeholders: placeholders,
+            warnings: warnings
+        });
     }
 }
 
